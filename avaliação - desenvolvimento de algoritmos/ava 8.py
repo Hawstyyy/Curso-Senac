5dc347eec6e4221407a29f0cac836208c3e568b6
Hawstyyy <hawstyyy@proton.me>
2024-05-17 10:23:24 -0400

    Update ava 8.py

--- avaliação - desenvolvimento de algoritmos/ava 8.py
+++ avaliação - desenvolvimento de algoritmos/ava 8.py
@@ -1,5 +1,29 @@
-higiene = ['papel i']
-for i in higiene:
-  higiene[i] = 3.00
+func_log = ['Carlos', '123','1/1/1', '123.123.123-12']
 
-print(higiene)
+while True:
+  ator = input("""
+  +===================================================+
+  |                 Bem-Vindo ao mercado!             |
+  +===================================================+
+  | C - Cadastro e login de cliente
+  | F - Login de funcionário
+  +===================================================+
+  - """)
+
+  if ator == "C" or ator == "c":
+    print("\n!!!!!!!!!!!!!!!!! Bem - vindo cliente !!!!!!!!!!!!!!!!!\n")
+    nome_cliente = input("- Insira seu nome completo: ")
+    cpf = input("- Insira o seu cpf: ")
+
+  elif ator == "F" or ator == "f":
+    print("\n!!!!!!!!!!!!!!!!! Bem - vindo funcionário !!!!!!!!!!!!!!!!!\n")
+    nome_f = input("- Insira o seu nome completo: ")
+    matricula = input("- Insira a sua matrícula: ")
+    nasc = input("- Insira a sua data de nascimento: ")
+    cpf_f = input("- Insira o seu cpf: ")
+
+    if nome_f == func_log[0] and matricula == func_log[1] and nasc == func_log[2] and cpf_f == func_log[3]:
+      print("\n!-! Login realizado com sucesso !-!\n")
+      break
+    else:
+      print("\n!-! Login incorreto !-!\n")

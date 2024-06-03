97fbe64320fb5e6c6ace107fae828d25a502c87f
Hawstyyy <hawstyyy@proton.me>
2024-06-03 07:53:25 -0400

    update

---
+++ UC - Analisar programação estruturada e orientada a objetos/a/atividade 01.py
@@ -0,0 +1,7 @@
+# Crie uma função que necessite de três argumentos, e que imprima o produto desses três argumentos.
+
+def funcao_poggers(numero1,numero2,numero3):
+  resultado = (numero1+numero2+numero3)
+  print(resultado)
+
+funcao_poggers(1,2,3)
--- UC - Desenvolvimento de algoritmos/lista de dict/atividade 16.py
+++ UC - Desenvolvimento de algoritmos/lista de dict/atividade 16.py
@@ -1,2 +1,3 @@
 # Crie um jogo de forca onde o programa escolhe uma palavra aleatória e o usuário
 # tenta adivinhar as letras. Use um dicionário para armazenar as palavras.
+

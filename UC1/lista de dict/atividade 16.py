5a300930c9b1accb5ef1b1267c796b724fdcb1cb
Hawstyyy <hawstyyy@proton.me>
2024-06-03 07:38:51 -0400

    arrumando pasta

---
+++ UC1/lista de dict/atividade 16.py
@@ -0,0 +1,2 @@
+# Crie um jogo de forca onde o programa escolhe uma palavra aleatória e o usuário
+# tenta adivinhar as letras. Use um dicionário para armazenar as palavras.
---
+++ UC1/lista de dict/atividade 17.py
@@ -0,0 +1,9 @@
+# Crie um dicionário com títulos de filmes e seus anos de lançamento. Peça ao
+# usuário para digitar um título e exiba o ano de lançamento
+
+# dict_filmes = {'A Era do Gelo: As Aventuras de Buck':2022, ''}
+
+def teste(teste):
+  print('teste')
+
+teste(" ")

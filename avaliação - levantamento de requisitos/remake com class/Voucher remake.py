e62c2e0dfd2b34339a87703d59677e91ea7e2bab
Enzo Guenka Lopez <hawstyyy@proton.me>
2024-07-23 12:49:01 -0400

    remake voucher

--- avaliação - levantamento de requisitos/remake com class/Voucher remake.py
+++ avaliação - levantamento de requisitos/remake com class/Voucher remake.py
@@ -4,31 +4,33 @@ registro = Registro
 banco = open("banco.txt", "r")
 linhas = banco.readlines()
 
-class UI(registro):
-  def __init__(self, codigo, nome, quantidade):
-    super().__init__(codigo, nome, quantidade)
+while True:
+  print('\n+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -')
+  for line in linhas:
+    campos = line.strip().split(',')
 
-  while True:
-    print('+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -')
-    for line in linhas:
-      campos = line.strip().split(',')
+    if len(campos) == 3:
+      codigo = campos[0]
+      nome = campos[1]
+      quantidade = campos[2]
+      print(f"| {codigo} - {nome} - {quantidade}")
+  banco.close()
+  print('+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -')
 
-      if len(campos) == 3:
-        codigo = campos[0]
-        nome = campos[1]
-        quantidade = campos[2]
-        print(f"| {codigo} - {nome} - {quantidade}")
-    banco.close()
-    print('+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -')
+  operacao = int(input("""
+  + - Selecione a sua operação desejada - 
+  | 1 - Entrada no estoque
+  | 2 - Saída no estoque
+  | 3 - Relatório do estoque
+  | 4 - Registro de produto
+  | 5 - Sair
+        - """))
+  if operacao == 'RG':
+    codigo = int(input('- Insira o código do produto: '))
+    nome = input('-Insira o nome do produto: ')
+    check = registro.check(codigo, nome)
 
-    operacao = input("""
-  + Selecione a sua operação desejada - 
-  | E - Entrada no estoque
-  | S - Saída no estoque
-  | R - Relatório do estoque
-  | RG - Registro de produto
-  | X - Sair
-        - """).upper()
-    if operacao == 'RG':
-      codigo = int(input('- Insira o código do produto: '))
-      nome = input('-Insira o nome do produto: ')
+    if check == True:
+      print("- Código ou nome já existente")
+    else:
+      print('a')
--- avaliação - levantamento de requisitos/remake com class/registro.py
+++ avaliação - levantamento de requisitos/remake com class/registro.py
@@ -2,16 +2,14 @@ banco = open("banco.txt", "r")
 linhas = banco.readlines()
 
 class Registro:
-  def __init__(self, codigo, nome, quantidade):
+  def __init__(self, codigo, nome):
     self.codigo = int(self.codigo)
     self.nome = self.nome
-    self.quantidade = int(self.quantidade)
 
-  def check(self, codigo, nome, quantidade):
+  def check(self, codigo, nome):
     for line in linhas:
       campos = line.strip().split(',')
-
-      if len(campos) == 3:
-        codigo = campos[0]
-        nome = campos[1]
-    if self.codigo == codigo:
+      if self.nome in campos and self.codigo in campos or self.codigo in campos:
+        return True
+      else:
+        return False

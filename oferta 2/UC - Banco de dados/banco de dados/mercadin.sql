7e6e6c096bf836c5e82498a0cbd6afdb6ceb4d53
Enzo Guenka Lopez <hawstyyy@proton.me>
2024-08-19 12:06:15 -0300

    atividade dia 19/08

--- oferta 2/UC - Banco de dados/banco de dados/mercadin.sql
+++ oferta 2/UC - Banco de dados/banco de dados/mercadin.sql
@@ -2,7 +2,7 @@ CREATE DATABASE Mercadin;
 USE Mercadin;
 
 CREATE TABLE EX2_CLIENTE(
-	codcliente INT,
+	codcliente int auto_increment,
     nome VARCHAR(100) NOT NULL,
     datanascimento VARCHAR(100) NOT NULL,
     cpf VARCHAR(14) NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE EX2_CLIENTE(
 );
 
 CREATE TABLE EX2_PEDIDO(
-	codpedido INT,
+	codpedido INT auto_increment,
     codcliente INT,
     datapedido VARCHAR(100) NOT NULL,
     nf VARCHAR(100) NOT NULL,
@@ -20,14 +20,14 @@ CREATE TABLE EX2_PEDIDO(
 );
 
 create table EX2_PRODUTO(
-	codproduto INT,
+	codproduto INT auto_increment,
     descricao varchar(100) not null,
     quantidade int not null,
     primary key (codproduto)
     );
 
 CREATE TABLE EX2_ITEMPEDIDO(
-	codpedido INT,
+	codpedido INT auto_increment,
     numeroitem INT,
     valorunitario FLOAT not null,
     quantidade INT not null,
@@ -38,17 +38,50 @@ CREATE TABLE EX2_ITEMPEDIDO(
 );
 
 create table EX2_LOG(
-	codlog INT,
+	codlog INT auto_increment,
     data varchar(50),
     descricao varchar(100),
     primary key (codlog)
 );
 
 create table EX2_REQUISICAO_COMPRA(
-	codrequisicaocompra int,
+	codrequisicaocompra int auto_increment,
     codproduto int,
     data varchar(100) not null,
     quantidade int not null,
     primary key (codrequisicaocompra),
     foreign key (codproduto) references EX2_PRODUTO(codproduto)
 );
+
+insert into EX2_CLIENTE (nome, datanascimento, cpf) values ("Braiani", "1991-10-28", "123.123.123-12");
+select * from EX2_ITEMPEDIDO;
+
+insert into EX2_CLIENTE (nome, datanascimento, cpf) values ("Cliente 3", "1234-12-12", "123.123.123-12"), 
+("Cliente 4", "1234-12-12", "123.123.123-12"), 
+("Cliente 5", "1234-12-12", "123.123.123-12"), 
+("Cliente 6", "1234-12-12", "123.123.123-12"), 
+("Cliente 7", "1234-12-12", "123.123.123-12"), 
+("Cliente 8", "1234-12-12", "123.123.123-12"), 
+("Cliente 9", "1234-12-12", "123.123.123-12"), 
+("Cliente 10", "1234-12-12", "123.123.123-12");
+
+insert into EX2_PRODUTO (descricao, quantidade) values ("produto", "10"),
+("produto", "10"), 
+("produto", "10"), 
+("produto", "10"), 
+("produto", "10"), 
+("produto", "10"), 
+("produto", "10"), 
+("produto", "10");
+
+insert into EX2_PEDIDO (codcliente ,datapedido, nf, valortotal) values (1,"2024-12-12", "123", 1000.00),
+(2,"2024-12-12", "123", 1000.00), 
+(3,"2024-12-12", "123", 1000.00), 
+(4,"2024-12-12", "123", 1000.00), 
+(5,"2024-12-12", "123", 1000.00);
+
+insert into EX2_ITEMPEDIDO (numeroitem, valorunitario, quantidade, codproduto) values (1, 10.00, 10, 1),
+(2, 10.00, 10, 2), 
+(3, 10.00, 10, 3), 
+(4, 10.00, 10, 4), 
+(5, 10.00, 10, 5);

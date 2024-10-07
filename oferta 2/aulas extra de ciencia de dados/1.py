785d0fea9558ca123ce9f6ac12befdeb6da9f599
Hawstyyy <hawstyyy@proton.me>
2024-10-07 08:39:18 -0400

    atividades dia 07/10

---
+++ oferta 2/aulas extra de ciencia de dados/1.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os,sys
+
+def basePath():
+  return os.path.dirname(os.path.abspath(sys.argv[0]))
+
+df = pd.read_csv('https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv')
+df.set_index('PassengerId', inplace=True)
+
+print(df.columns)
+print()
+print(df.loc[[1,2], ['Name', 'Sex', 'Age', 'Survived']])
+print()
+print(df.loc[1:5])
+print()
+print(df.loc[1:7:2, ['Name','Sex','Age', 'Survived']])
+print()
+print(df.query('(Age > 50 | Age < 20) & Sex=="male"').head(10))
+df.to_csv(f'{basePath()}/dataset.csv',sep=';',index=False, encoding='utf-8-sig')
+print()
+print()
---
+++ oferta 2/aulas extra de ciencia de dados/atividades.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import os, sys
+
+def basePath():
+  return os.path.dirname(os.path.abspath(sys.argv[0]))
+
+def Save(query, nome):
+  parameter = df.query(query)
+  parameter.to_csv(f'{basePath()}/{nome}', sep=';',index=False, encoding='utf-8-sig')
+
+df = pd.read_csv(f'https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv')
+df.set_index('PassengerId', inplace=True)
+
+# Quantas crianças abaixo de 10 anos sobreviveram?
+
+Save(query='Age < 10 & Survived==1', nome='underten.csv')
+
+# Quantas mulheres sobreviveram?
+
+Save(query='Sex=="female" & Survived==1', nome='women.csv')
+
+# Quantos homens sobreviveram?
+
+Save(query='Sex=="male" & Survived==1', nome='men.csv')
+
+# Quantos idosos acima de 50 anos sobreviveram?
+
+Save(query='Age > 50 & Survived==1', nome='eldery.csv')
+
+# Quantas crianças abaixo de 12 anos do sexo feminnino sobreviveram?
+
+Save(query='Age < 12 & Sex=="female"', nome='children.csv')

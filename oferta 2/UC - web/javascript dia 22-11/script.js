14969f162edfe315dce0affd2e65290d3d8a862e
Enzo Lopez <enzo54099426@aluno.ms.senac.br>
2024-11-22 08:33:48 -0400

    atividade dia 22/11

---
+++ oferta 2/UC - web/javascript dia 22-11/script.js
@@ -0,0 +1,54 @@
+// window.alert('alert')
+// window.confirm('confirm')
+// window.prompt('prompt da silva')
+
+// let x = BigInt(218331983792132132);
+// console.log(x);
+
+// let tipo = typeof x;
+// console.log(tipo);
+
+// let nota = 10.5;
+// console.log(nota.toFixed(2).replace('.',','));
+
+// let nome = 'nome doido';
+// console.log('sem crase: ' + nome);
+// console.log(`com crase: ${nome}`)
+
+// let nomeAlert = window.prompt('bota o teu nome ai');
+// window.alert(`bem-vindo ${nomeAlert}`);
+
+// let x = 10;
+// while (x < 50){
+//   x++;
+//   console.log(x);
+// };
+
+// logicos - &&(and), ||(or), !(not)
+
+// operador ternario - condico ?(if) expressao1 :(else) expressao2
+
+// let media = 4
+// media >= 7 ? console.log('aprovado') : console.log('reprovado')
+
+// let nomeAlert = window.prompt('bota o teu nome ai');
+// document.write(`bem-vindo ${nomeAlert}`);
+
+// document.write(Date());
+
+// window.alert('sudo rm -rf /');
+
+// let string = 'hello';
+// console.log(string.length);
+// console.log(string.charAt(0));
+// console.log(string.toLowerCase())
+// console.log(string.toUpperCase())
+
+// let stringbranco = 'hello world'
+// console.log(stringbranco.trim())
+// console.log(stringbranco.repeat(2))
+
+// function funcao(p1,p2){ 
+//   return p1 * p2
+// }
+// funcao(1,2)
